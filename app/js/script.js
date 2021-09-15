c3eee8a2c4354b98ca96edae0b3b5369c63ec7b7
ricky <ricky.chaggar@gmail.com>
2021-09-15 17:00:15 +0100

    started adding js for functionality. variables and event listeners added

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,23 @@
-console.log("JS is loading");
+console.log("Javascript loaded");
+
+// Initialize variables
+
+let bill = 0,
+  tip = 0,
+  people = 0;
+
+// Get HTML elements
+
+const billAmount = document.querySelector("#bill_amount");
+const percs = document.querySelectorAll(".button__perc");
+const customTip = document.querySelector("#custom_tip");
+const noPeople = document.querySelector("#people_amount");
+const tipPerPerson = document.querySelector("#tip_per_person");
+const totalPerPerson = document.querySelector("#total_per_person");
+
+// Add event listeners
+
+billAmount.addEventListener("keyup", (e) => {
+  bill = billAmount.value;
+  console.log(bill);
+});
